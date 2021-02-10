96c99a74947bde307fbb00d30cf01a56c791bf77
Marc <marc.ferraro1@gmail.com>
2021-02-10 14:19:53 -0600

    commit tree ff68ddf99bfb83f3ed22c26e07bda238d36c9ce5

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+
+    let phrase = tutorial.split(' ')
+    
+    const upcasePhrase = phrase.map(word => word[0].toUpperCase() + word.substring(1)).join(" ")
+    return upcasePhrase
+
+  })
 }
